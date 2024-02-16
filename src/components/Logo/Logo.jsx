f17dd1e8d0e263c1d3bd5e50d1d9bb1f46802dd6
Inamul Haq <ihaq81285@gmail.com>
2024-02-16 20:36:10 +0530

    #

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import LogoImage from "../../assets/qtify-logo.svg";
+import Styles from '../Logo/Logo.module.css'
 function Logo(){
     return(
-        <img src={LogoImage} alt="Logo" width={67} />
+        <img src={LogoImage} alt="Logo" width={67} className={Styles.logo}/>
     );
 }
 
