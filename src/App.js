b482017a90a218238c437a6c095baa0a0c3293d6
Inamul Haq <ihaq81285@gmail.com>
2024-03-06 21:27:51 +0530

    Qtify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import HeroSection from './components/HeroSection/HeroSection';
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { fetchTopAlbums, fetchSongs, fetchNewAlbums } from "./api/api";
 
 function App() {
-  return (
-    <>
-    <Navbar/>
-    <HeroSection/>
-</>
+    const [data, setData] = useState({});
 
+    const generateData = (key, source) => {
+        source().then((data) => {
+            setData((prevData) => {
+                return{...prevData, [key]: data}; 
+            })
+        })
+    }
+
+    useEffect(() => {
+        generateData("topAlbums", fetchTopAlbums);
+        generateData("newAlbums", fetchNewAlbums);
+        generateData("songs", fetchSongs);
+    }, []);
+
+    const {topAlbums = [], newAlbums = [], songs = []} = data;
+  return (
+    <>  
+      <div>
+      <Navbar/>
+      <Outlet context={{data: {topAlbums, newAlbums, songs}}}/>
+      </div>
+    </>
   );
 }
 
